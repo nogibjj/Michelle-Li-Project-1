702ddd2d886a91a6be2d3f73a28a6bf68de1bd61
Michelle Li <70456530+michellejieli@users.noreply.github.com>
2022-09-12 03:15:36 +0000

    updated hellow.py

--- hello.py
+++ hello.py
@@ -1,6 +1,34 @@
+import random
 # write a hello function that prints hello world
 def hello():
     print("Hello World!")
 
 # invoke hello function
 hello()
+
+# write decorator function that logs input and outputs and times it to a function
+def log(func):
+    def wrapper(*args, **kwargs):
+        print("Input: {} {}".format(args, kwargs))
+        result = func(*args, **kwargs)
+        print("Output: {}".format(result))
+        return result
+    return wrapper
+
+# invoke decorator function
+@log
+def hello(x):
+    print("Hello World! {x}")
+
+hello(3) 
+
+# write a function that yields a generator that randomly picks 3 types of fruits
+def fruit_generator():
+    fruits = ['apple', 'orange', 'banana']
+    while True:
+        yield random.choice(fruits)
+
+# invoke fruit generator
+for i in range(5):
+    print(next(fruit_generator()))
+
